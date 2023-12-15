b3b618a4b225462798dbb0c90de386854be51b47
tynka <tynai.s.@gmail.com>
2023-12-15 12:21:34 +0600

    some modifications

--- CSproject.py
+++ CSproject.py
@@ -8,12 +8,12 @@ def calculate_EAR(eye):
     A = distance.euclidean(eye[1], eye[5])
     B = distance.euclidean(eye[2], eye[4])
     C = distance.euclidean(eye[0], eye[3])
-    ear_aspect_ratio = (A + B) / (2.0 * C)
-    return ear_aspect_ratio
+    eye_aspect_ratio = (A + B) / (2.0 * C)
+    return eye_aspect_ratio
 
 
 cap = cv2.VideoCapture(0)
-hog_face_detector = dlib.get_frontal_face_detector()
+face_detector = dlib.get_frontal_face_detector()
 dlib_facelandmark = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
  
 
@@ -21,7 +21,7 @@ while True:
     _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = hog_face_detector(gray)
+    faces = face_detector(gray)
     for face in faces:
 
         face_landmarks = dlib_facelandmark(gray, face)
